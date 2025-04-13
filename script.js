63db8c09617a527fc77b115bdc0ba9cdccc9c1a0
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-04-13 14:05:47 +0700

    Dynamic div size
    I want to change the size of the divs dynamically
    so I set the size as a variable in the script,
    and then change the container's sizes accordingly.
    
    This adds inline style to the HTML tho, which is
    really bad and ugly, and yucky, and I don't want it.
    
    Also I should delete setAttribute.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector(".container");
 
+const itemSize = 50;
+
 for (let width = 0; width < 16; width++) {
     for (let height = 0; height < 16; height++) {
         const item = document.createElement("div");
+
+        item.style.width = itemSize + "px";
+        item.style.height = itemSize + "px";
+        item.style.backgroundColor = "white";
 
         // set class="item", not style=.item
         item.setAttribute("class", "item");
@@ -10,3 +16,6 @@ for (let width = 0; width < 16; width++) {
         container.appendChild(item);
     }
 }
+
+container.style.width = itemSize * 16 + "px";
+container.style.width = itemSize * 16 + "px";
