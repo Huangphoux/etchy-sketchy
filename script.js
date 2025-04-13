56e5c3f8073d1de91d2231521073f3db61a8e56b
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-04-13 14:16:56 +0700

    Add hover effect for items

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 
-const itemSize = 50;
+const itemSize = 30;
 
 for (let width = 0; width < 16; width++) {
     for (let height = 0; height < 16; height++) {
@@ -8,7 +8,8 @@ for (let width = 0; width < 16; width++) {
 
         item.style.width = itemSize + "px";
         item.style.height = itemSize + "px";
-        item.style.backgroundColor = "white";
+        item.style.backgroundColor =
+            "color-mix(in srgb, Canvas, CanvasText 30%)";
 
         // set class="item", not style=.item
         item.setAttribute("class", "item");
@@ -18,4 +19,12 @@ for (let width = 0; width < 16; width++) {
 }
 
 container.style.width = itemSize * 16 + "px";
-container.style.width = itemSize * 16 + "px";
+container.style.height = itemSize * 16 + "px";
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "white";
+    });
+});
